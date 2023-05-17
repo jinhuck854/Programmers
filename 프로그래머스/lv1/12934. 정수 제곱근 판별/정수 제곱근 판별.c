8f02776f375d0a73bb9e97486b10ab8ee7fa9c47
jinhuck854 <121598818+jinhuck854@users.noreply.github.com>
2023-05-17 19:03:13 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.01 ms, Memory: 4.52 MB -BaekjoonHub

--- 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.c
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.c
@@ -5,7 +5,7 @@
 long long solution(long long n) {
     long long answer = 0;
     
-    for(long long i = 0; i<=n; i++){
+    for(long long i = 0; i<=n; i++){ // for문 사용해서 0부터 시작, 조건 맞으면은 return answer
         if(i * i == n){
             return answer = (i + 1)*(i + 1);
         }
