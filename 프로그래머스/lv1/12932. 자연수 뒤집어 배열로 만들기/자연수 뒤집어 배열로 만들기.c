0d73f20ef9a9c8fca59003bcee962e3150785e1d
jinhuck854 <121598818+jinhuck854@users.noreply.github.com>
2023-05-17 18:47:07 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/lv1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.c
+++ 프로그래머스/lv1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.c
@@ -4,8 +4,13 @@
 
 int* solution(long long n) {
     // 리턴할 값은 메모리를 동적 할당해주세요.
-    int* answer = (int*)malloc(sizeof(int) * 12);
-    // Why long long?
+    int* answer = (int*)malloc(sizeof(int) * 16);
+    /*
+    int* answer = (int*)malloc(sizeof(int));
+        -> segment 오류
+    이유 : int = 4바이트, long long int = 8바이트.
+        -> 
+    */
     
     for(int i = 0; n != 0; i++){
         answer[i] = n%10; // 10의 자리로 나누면 1의 자리만 남음
