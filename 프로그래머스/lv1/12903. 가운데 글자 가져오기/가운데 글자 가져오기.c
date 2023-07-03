65b3df89cffa5cd58cd82bb1830528184bcad388
jinhuck854 <121598818+jinhuck854@users.noreply.github.com>
2023-07-03 14:43:59 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.c
+++ 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.c
@@ -4,16 +4,14 @@
 #include <string.h>
 
 // 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
-char* solution(const char* s) {
-    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
-    
-    char* answer = (char*)malloc(sizeof(char) * 2);
+char* solution(const char* s) {    
+    char* answer = (char*)malloc(strlen(s)); // 문자열 길이만큼 동적 할당
     int len = strlen(s);
     
     if(len % 2 == 0){ // 짝수
         answer[0] = s[(len/2)-1];
         answer[1] = s[(len/2)];
-        answer[2] = NULL;
+        answer[2] = '\0';
     }
     else{ // 홀수
         answer[0] = s[len/2];
