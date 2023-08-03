adc4e7646619254e5b557665951658e86fe81b20
Lee_jinhyeok <121598818+jinhuck854@users.noreply.github.com>
2023-08-03 16:05:24 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.02 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+long long solution(int n) {
+    long long answer = 0;
+    long long* array = (long long*)malloc(sizeof(long long) * (n*n));
+    
+    array[0] = 0;
+    array[1] = 1;
+    array[2] = 2;
+    
+    if(n == 1)
+        return 1;
+    else if(n == 2)
+        return 2;
+    else{ // 3이상
+        for(int i = 3; i<=n; i++){
+            array[i] = (array[i-1] + array[i-2]) % 1234567;
+            answer = array[i];
+            //answer = (array[i-1] + array[i-2]) % 1234567;
+            //printf("i-1 : %d, i-2 : %d\n", array[i-1], array[i-2]);
+        }
+    }
+    
+    return answer;
+}
