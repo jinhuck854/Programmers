585cf9a23c45ab63205cfb240841c9346dfb64c1
Lee_jinhyeok <121598818+jinhuck854@users.noreply.github.com>
2023-08-31 17:17:32 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+int swap(int *array[], k){ // 내림차순
+    for(int i = 0; i< k-1; i++){
+        int index = i;
+        for(int j = i+1; j<k; j++){
+            if(array[j] > array[index])
+                index = j;
+        }
+        int temp = array[index];
+        array[index] = array[i];
+        array[i] = temp;
+    }
+    /*
+    for(int i = 0; i<k; i++)
+        printf("%d %d\n", i, array[i]);
+    printf(" end\n");
+    */
+}
+
+int* solution(int k, int score[], size_t score_len) {
+    int* answer = (int*)malloc(score_len * sizeof(int));
+    // 배열 선언 후 초기화 
+    int* array[score_len];
+    
+    for(int i = 0; i<score_len; i++){
+        array[i] = score[i];
+        
+        swap(array, i+1); // 선택정렬
+        
+        if(i < k){ // 0~k번째까지 // k=3, 0 1 2
+            answer[i] = array[i];
+        }
+        else{ // k번째~score_len까지
+            answer[i] = array[k-1];
+        }
+        
+    }
+    return answer;
+}
